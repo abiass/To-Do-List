27f4df092455f971f3f71424f8e64c49a6aa67ab
abias <abias.melo@hotmail.com>
2024-03-20 22:57:10 -0300

    finalização

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function validateIfExistsNewTask() {
 function newTask() {
     let input = document.getElementById('input-new-task')
     input.style.border = ''
-    input.value = ''
+
 
     //validation
     if(!input.value) {
@@ -29,14 +29,17 @@ function newTask() {
         localStorage.setItem(localStorageKey,JSON.stringify(values))
         showValues()
     }
+    input.value = ''
 }
 
 function showValues() {
     let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
     let list = document.getElementById('to-do-list')
     list.innerHTML = ''
-    for (let i = 0; i <values.length; i++) {
-        list.innerHTML += `<li>${values[i]['name']}<button id='btn-ok' onclick="removeItem"("${values[1]}")>OK</button></li>`
+    for(let i = 0; i < values.length; i++) {
+        list.innerHTML += `<li>${values[i]['name']}<button id='btn-ok' onclick='removeItem("${values[i]['name']}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+      </svg></button></li>`
     }
 }
 
